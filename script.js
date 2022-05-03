c6cc5b98062c720e999de537455ab931d4ba98af
xhevo <xhevat@livelike.com>
2022-05-03 12:06:22 +0200

    added image poll and image quiz implementations

--- script.js
+++ script.js
@@ -1,5 +1,230 @@
+var imagePollWidgetPayload = {
+    "event": "image-poll-created",
+    "widgetPayload": {
+        "id": "06162a84-125b-40d3-8dfc-0817ec291738",
+        "url": "https://cf-blast.livelikecdn.com/api/v1/image-polls/06162a84-125b-40d3-8dfc-0817ec291738/",
+        "kind": "image-poll",
+        "program_id": "9baf1962-f7db-43b9-ae8e-b84f1bf31988",
+        "client_id": "jV2LSongmdccPRL65M8GC6Z8lTievPsRsqsoSMiq",
+        "created_at": "2022-05-03T08:44:07.679Z",
+        "published_at": "2022-05-03T08:44:08.108Z",
+        "scheduled_at": "2022-05-03T08:44:08.035Z",
+        "interactive_until": null,
+        "question": "Which country won the world cup 202O in Brazil?",
+        "timeout": "P0DT00H01M00S",
+        "options": [
+            {
+                "id": "21475195-5263-4e53-aa4a-607850f80aff",
+                "description": "Flag Brazil",
+                "image_url": "https://cf-blast-storage.livelikecdn.com/assets/ec2ff86e-6538-496a-a130-335d4ddc5438.png",
+                "vote_count": 0,
+                "vote_url": "https://blastrt-prod-us-east-1.livelikecdn.com/api/v1/widget-interactions/image-poll/06162a84-125b-40d3-8dfc-0817ec291738/options/21475195-5263-4e53-aa4a-607850f80aff/votes/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbmNyeXB0ZWQiOiJnQUFBQUFCaWNPdFlpcDliVW44YlItZURpLTFNRE96bDBKZFNvQ1dfS2dEN2FEc2hYejkxWHpzMWtVZ212aE16Q1VfeEltRzlqTHdHdFZQcmFjQUk4SThxY19SVFVzT3pOQT09IiwiaXNzIjoiYmxhc3QiLCJpYXQiOjE2NTE1Njc0NDh9.SVtLDXbLcaWGoVtifZiNU7Xc-mdjleCQe47N-udjX1U/",
+                "translations": {},
+                "translatable_fields": [
+                    "description"
+                ]
+            },
+            {
+                "id": "307ef4c0-31fd-4b12-83e5-f2b67371fea0",
+                "description": "England Flag",
+                "image_url": "https://cf-blast-storage.livelikecdn.com/assets/4c7e800a-ad8c-4264-a9df-4eeab6ca70fc.png",
+                "vote_count": 0,
+                "vote_url": "https://blastrt-prod-us-east-1.livelikecdn.com/api/v1/widget-interactions/image-poll/06162a84-125b-40d3-8dfc-0817ec291738/options/307ef4c0-31fd-4b12-83e5-f2b67371fea0/votes/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbmNyeXB0ZWQiOiJnQUFBQUFCaWNPdFljd3U2UjUyVFNQdGFRWE5SVUozZS03YWExVkRMZk5fTVJNUEc2NjhUR1FVc3YwX3Vnd3o2OVdnbnl0b3ZRWjdTR3NsS1d4OV9sTDFUS3pOeGcwWGctQT09IiwiaXNzIjoiYmxhc3QiLCJpYXQiOjE2NTE1Njc0NDh9.SbT-q9zopmBbI4iE3NHDN6f4kGPb3ns-vUakvd1QsrQ/",
+                "translations": {},
+                "translatable_fields": [
+                    "description"
+                ]
+            }
+        ],
+        "subscribe_channel": "program.widget.9baf1962-f7db-43b9-ae8e-b84f1bf31988",
+        "program_date_time": null,
+        "publish_delay": "P0DT00H00M00S",
+        "interaction_url": "https://cf-blast.livelikecdn.com/api/v1/image-polls/06162a84-125b-40d3-8dfc-0817ec291738/interactions/",
+        "impression_url": "https://blastrt-prod-us-east-1.livelikecdn.com/api/v1/widget-impressions/image-poll/06162a84-125b-40d3-8dfc-0817ec291738/",
+        "impression_count": 0,
+        "unique_impression_count": 0,
+        "engagement_count": 0,
+        "engagement_percent": "0.000",
+        "status": "published",
+        "created_by": {
+            "id": "b3affd11-5bef-49fc-b430-03525a63995a",
+            "name": null,
+            "image_url": null
+        },
+        "schedule_url": "https://cf-blast.livelikecdn.com/api/v1/image-polls/06162a84-125b-40d3-8dfc-0817ec291738/schedule/",
+        "rewards_url": null,
+        "translations": {},
+        "translatable_fields": [
+            "question"
+        ],
+        "reactions": [],
+        "custom_data": null,
+        "sponsors": [],
+        "widget_interactions_url_template": "https://cf-blast.livelikecdn.com/api/v1/profiles/{profile_id}/widget-interactions/?image_poll_id=06162a84-125b-40d3-8dfc-0817ec291738",
+        "widget_attributes": [],
+        "timetoken": "16515674482633900"
+    },
+    "programId": "9baf1962-f7db-43b9-ae8e-b84f1bf31988"
+};
+var imageQuizWidgetPayload = {
+    "id": "e79bdde9-075c-4585-b2e3-17ba7220c534",
+    "url": "https://cf-blast.livelikecdn.com/api/v1/image-quizzes/e79bdde9-075c-4585-b2e3-17ba7220c534/",
+    "program_id": "9baf1962-f7db-43b9-ae8e-b84f1bf31988",
+    "client_id": "jV2LSongmdccPRL65M8GC6Z8lTievPsRsqsoSMiq",
+    "created_at": "2022-05-03T09:52:21.636Z",
+    "kind": "image-quiz",
+    "published_at": "2022-05-03T09:52:33.047Z",
+    "scheduled_at": "2022-05-03T09:52:31.445Z",
+    "interactive_until": null,
+    "question": "1",
+    "timeout": "P0DT00H05M00S",
+    "choices": [
+        {
+            "id": "62074761-ddfc-4e3d-a11c-57e31622fcc7",
+            "description": "Neymar",
+            "is_correct": false,
+            "image_url": "https://cf-blast-storage.livelikecdn.com/assets/3f92ada1-7441-4847-8b8c-734ed840efa1.png",
+            "answer_url": "https://blastrt-prod-us-east-1.livelikecdn.com/api/v1/widget-interactions/image-quiz/e79bdde9-075c-4585-b2e3-17ba7220c534/choices/62074761-ddfc-4e3d-a11c-57e31622fcc7/answers/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbmNyeXB0ZWQiOiJnQUFBQUFCaWNQdGgtdjZxNDlWeDZpZEREYXZkUjFOclVmdXVkRmE4OFVEYTNzNV90akd3eEF5bFF2MVp0YnB6Y0U4NW9XSGRTeXF6Zm9qVlB5VmNEajB6TWZCeG05YjYwZz09IiwiaXNzIjoiYmxhc3QiLCJpYXQiOjE2NTE1NzE1NTN9.ITgVI8MKThvv-d03rSYQfofyiNCnbIFUbeJJWAYjT6Q/",
+            "answer_count": 0,
+            "translations": {},
+            "translatable_fields": [
+                "description"
+            ]
+        },
+        {
+            "id": "36c6fd6e-41bc-449a-a6f7-f8c14239cc9f",
+            "description": "Flag Brazil",
+            "is_correct": false,
+            "image_url": "https://cf-blast-storage.livelikecdn.com/assets/ec2ff86e-6538-496a-a130-335d4ddc5438.png",
+            "answer_url": "https://blastrt-prod-us-east-1.livelikecdn.com/api/v1/widget-interactions/image-quiz/e79bdde9-075c-4585-b2e3-17ba7220c534/choices/36c6fd6e-41bc-449a-a6f7-f8c14239cc9f/answers/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbmNyeXB0ZWQiOiJnQUFBQUFCaWNQdGgwMUFScEFiZjFzUmlwOGtUV3llbWQxX0cxVDdiQ0lkMENFWUFXc0h5LUluSm1MODhuUzE1Ykd6dzlaUnFJU01TMGpBblFWRmxobWVrU292Q2VpdU1EUT09IiwiaXNzIjoiYmxhc3QiLCJpYXQiOjE2NTE1NzE1NTN9.14Ed2nCpgUwiCc0In4BbX7eUedK3qvkJ22AbEpbt-SM/",
+            "answer_count": 0,
+            "translations": {},
+            "translatable_fields": [
+                "description"
+            ]
+        }
+    ],
+    "subscribe_channel": "program.widget.9baf1962-f7db-43b9-ae8e-b84f1bf31988",
+    "program_date_time": null,
+    "publish_delay": "P0DT00H00M00S",
+    "interaction_url": "https://cf-blast.livelikecdn.com/api/v1/image-quizzes/e79bdde9-075c-4585-b2e3-17ba7220c534/interactions/",
+    "impression_url": "https://blastrt-prod-us-east-1.livelikecdn.com/api/v1/widget-impressions/image-quiz/e79bdde9-075c-4585-b2e3-17ba7220c534/",
+    "impression_count": 0,
+    "unique_impression_count": 0,
+    "engagement_count": 0,
+    "engagement_percent": "0.000",
+    "status": "published",
+    "created_by": {
+        "id": "b3affd11-5bef-49fc-b430-03525a63995a",
+        "name": null,
+        "image_url": null
+    },
+    "schedule_url": "https://cf-blast.livelikecdn.com/api/v1/image-quizzes/e79bdde9-075c-4585-b2e3-17ba7220c534/schedule/",
+    "rewards_url": null,
+    "translations": {},
+    "translatable_fields": [
+        "question"
+    ],
+    "reactions": [],
+    "custom_data": null,
+    "sponsors": [],
+    "earnable_rewards": [
+        {
+            "reward_item_id": "f412f658-fc53-4924-8fed-8103d81a826d",
+            "reward_item_name": "Web Demo Reward",
+            "reward_action_key": "quiz-correct",
+            "reward_item_amount": 1000
+        },
+        {
+            "reward_item_id": "f412f658-fc53-4924-8fed-8103d81a826d",
+            "reward_item_name": "Web Demo Reward",
+            "reward_action_key": "quiz-answered",
+            "reward_item_amount": 9
+        }
+    ],
+    "widget_interactions_url_template": "https://cf-blast.livelikecdn.com/api/v1/profiles/{profile_id}/widget-interactions/?image_quiz_id=e79bdde9-075c-4585-b2e3-17ba7220c534",
+    "widget_attributes": [],
+    "timetoken": "16515715535554870"
+};
+
+
+const createImagePollOption = (imageQuizOption) => {
+    return `
+<div class="col-md-6">
+    <div class="image-quiz-option">
+        <img class="image-quiz-option-img"
+            src="${imageQuizOption.image_url}"
+            alt="">
+            <p class="image-quiz-option-description">${imageQuizOption.description}</p>
+    </div>
+</div>
+`;
+};
+
+const getImagePollOptions = (options) => {
+    let result = "";
+    for (let i = 0; i < options.length; i++) {
+        result += createImagePollOption(options[i]);
+    }
+    return result;
+};
+
+const getImagePoll = (widget) => {
+    return `
+    <div class="widget-container">
+    <div class="widget-header">
+        <div class="row">
+            <span>${widget.question}</span>
+        </div>
+    </div>
+    <div class="widget-body">
+        <div class="image-quiz-container">
+            <div class="image-quiz-options">
+                <div class="row">
+                    ${getImagePollOptions(widget.options)}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    `;
+};
+
+const getWidgetsContainer = () => {
+    return document.querySelector(".widgets-container");
+};
+
+const cleanWidgetsContainer = () => {
+    let childNode = widgetsContainer.lastElementChild;
+    while (childNode) {
+        widgetsContainer.removeChild(childNode);
+        childNode = widgetsContainer.lastElementChild;
+    }
+};
+
+const addWidgetInContainer = (content) => {
+    var widgetsContainer = getWidgetsContainer();
+    widgetsContainer.innerHTML = content;
+};
+
+const setImageQuizWidget = (widget) => {
+    addWidgetInContainer(getImagePoll(widget));
+}
+
 const widgetHandler = (e) => {
-    console.log(e);
+    console.log(e.event);
+    console.log(e.widgetPayload);
+    if (e.event === "image-poll-created") {
+        console.log(e.widgetPayload.question);
+        setImageQuizWidget(e.widgetPayload);
+    }
+    if (e.event === "image-quiz-created") {
+        console.log(e.widgetPayload.question);
+        setImageQuizWidget({
+            options: e.widgetPayload.choices,
+            question: e.widgetPayload.question
+        });
+    }
 };
 
 const setupWidgetListener = () => {
