169d8a5433276eb7ca26e0885f4c28bf30669b0e
xhevo <xhevat@livelike.com>
2022-05-06 13:43:11 +0200

    updated cheer meter ui

--- themes/cheer-meter-theme.js
+++ themes/cheer-meter-theme.js
@@ -1,15 +1,147 @@
 const cheerMeterTheme = {
     header: {
-        fontColor: "000000",
+        borderColor: "00aaff",
+        borderWidth: 1,
+        fontColor: "ffffff",
+        fontSize: 70,
         background: {
-            color: "000000",
+            color: "00aaff",
             format: "fill"
-        }
+        },
+        margin: [0, 0, 0, 0],
     },
     body: {
         background: {
-            color: "000000",
+            color: "ffffff00",
             format: "fill"
         }
     },
+    "versus": {
+        "borderColor": "000000",
+        "fontWeight": "normal",
+        "borderRadius": [
+          0,
+          0,
+          0,
+          0
+        ],
+        "padding": [
+          0,
+          0,
+          0,
+          0
+        ],
+        "fontFamily": [
+          "sans-serif"
+        ],
+        "fontSize": 12,
+        "borderWidth": 0,
+        "background": {
+          "color": "00000000",
+          "format": "fill"
+        },
+        "fontColor": "000000",
+        "margin": [
+          0,
+          0,
+          0,
+          0
+        ]
+      },
+      sideBBar: {
+        "fontWeight": "normal",
+        "fontSize": 40,
+        "borderWidth": 0,
+        "background": {
+          "color": "575757",
+          "format": "fill"
+        },
+        fontColor: "ffffff",
+        margin: [
+          25,
+          0,
+          0,
+          0
+        ]
+      },
+      sideABar: {
+        "fontWeight": "normal",
+        "fontSize": 40,
+        "borderWidth": 0,
+        "background": {
+          "color": "00aaff",
+          "format": "fill"
+        },
+        fontColor: "000000",
+        margin: [
+          25,
+          0,
+          0,
+          0
+        ]
+      },
+      "sideAButton": {
+        "borderColor": "000000",
+        "fontWeight": "normal",
+        "borderRadius": [
+          0,
+          0,
+          0,
+          0
+        ],
+        "padding": [
+          0,
+          0,
+          0,
+          0
+        ],
+        "fontFamily": [
+          "sans-serif"
+        ],
+        "fontSize": 12,
+        "borderWidth": 0,
+        "background": {
+          "color": "00000000",
+          "format": "fill"
+        },
+        "fontColor": "000000",
+        "margin": [
+          50,
+          0,
+          0,
+          0
+        ]
+      },
+      "sideBButton": {
+        "borderColor": "000000",
+        "fontWeight": "normal",
+        "borderRadius": [
+          0,
+          0,
+          0,
+          0
+        ],
+        "padding": [
+          0,
+          0,
+          0,
+          0
+        ],
+        "fontFamily": [
+          "sans-serif"
+        ],
+        "fontSize": 12,
+        "borderWidth": 0,
+        "background": {
+          "color": "00000000",
+          "format": "fill"
+        },
+        "fontColor": "000000",
+        "margin": [
+          50,
+          0,
+          0,
+          0
+        ]
+      }
 };
