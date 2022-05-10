5ab95d20796c72a369e203e7ff4b6b8ad41b9cfd
tanyalivelike <67103999+tanyalivelike@users.noreply.github.com>
2022-05-10 14:04:10 +0530

    added leaderboard (#1)

--- script.js
+++ script.js
@@ -1,99 +1,140 @@
 const getWidgetsContainer = () => {
-    return document.querySelector(".widgets-container");
+  return document.querySelector('.widgets-container');
 };
 
 const cleanWidgetsContainer = () => {
-    let childNode = widgetsContainer.lastElementChild;
-    while (childNode) {
-        widgetsContainer.removeChild(childNode);
-        childNode = widgetsContainer.lastElementChild;
-    }
+  let childNode = widgetsContainer.lastElementChild;
+  while (childNode) {
+    widgetsContainer.removeChild(childNode);
+    childNode = widgetsContainer.lastElementChild;
+  }
 };
 
 const addWidgetInContainer = (content) => {
-    var widgetsContainer = getWidgetsContainer();
-    widgetsContainer.innerHTML = content;
+  var widgetsContainer = getWidgetsContainer();
+  widgetsContainer.innerHTML = content;
 };
 
 const setAlertWidget = (widget) => {
-    addWidgetInContainer(getAlert(widget));
+  addWidgetInContainer(getAlert(widget));
 };
 
 const setVideoAlertWidget = (widget) => {
-    addWidgetInContainer(getVideoAlert(widget));
+  addWidgetInContainer(getVideoAlert(widget));
 };
 
 const getLastPublishedWidget = async () => {
-    var response = await LiveLike.getPostedWidgets({ programId: programId });
-    if (response.widgets && response.widgets.length) {
-        return response.widgets[0];
-    };
-}
+  var response = await LiveLike.getPostedWidgets({ programId: programId });
+  if (response.widgets && response.widgets.length) {
+    return response.widgets[0];
+  }
+};
 
 const loadLastPublishedWidget = async () => {
-    const lastPublishedWidget = await getLastPublishedWidget();
-    if (lastPublishedWidget) {
-        widgetHandler(lastPublishedWidget);
-    }
-}
+  const lastPublishedWidget = await getLastPublishedWidget();
+  if (lastPublishedWidget) {
+    widgetHandler(lastPublishedWidget);
+  }
+};
 
 const widgetHandler = (widget) => {
-    console.log(widget);
-    if (widget.kind === "text-poll") {
-        addWidgetInContainer(createTextPollWidget(widget));
-    }
-    if (widget.kind === "text-quiz") {
-        addWidgetInContainer(createTextQuizWidget(widget));
-    }
-    if (widget.kind === "text-prediction") {
-        addWidgetInContainer(createTextPredictionWidget(widget))
-    }
-    if (widget.kind === "image-poll") {
-        addWidgetInContainer(createImagePollWidget(widget));
-    }
-    if (widget.kind === "image-quiz") {
-        addWidgetInContainer(createImageQuizWidget(widget));
-    }
-    if (widget.kind === "image-prediction") {
-        addWidgetInContainer(createImagePredictionWidget(widget));
-    }
-    if (widget.kind === "emoji-slider") {
-        addWidgetInContainer(getEmojiSlider(widget));
-    }
-    if (widget.kind === "cheer-meter") {
-        addWidgetInContainer(getCheerMeter(widget));
-    }
-    if (widget.kind === "alert") {
-        setAlertWidget(widget);
-    }
-    if (widget.kind === "video-alert") {
-        setVideoAlertWidget(widget);
-    }
+  console.log(widget);
+  if (widget.kind === 'text-poll') {
+    addWidgetInContainer(createTextPollWidget(widget));
+  }
+  if (widget.kind === 'text-quiz') {
+    addWidgetInContainer(createTextQuizWidget(widget));
+  }
+  if (widget.kind === 'text-prediction') {
+    addWidgetInContainer(createTextPredictionWidget(widget));
+  }
+  if (widget.kind === 'image-poll') {
+    addWidgetInContainer(createImagePollWidget(widget));
+  }
+  if (widget.kind === 'image-quiz') {
+    addWidgetInContainer(createImageQuizWidget(widget));
+  }
+  if (widget.kind === 'image-prediction') {
+    addWidgetInContainer(createImagePredictionWidget(widget));
+  }
+  if (widget.kind === 'emoji-slider') {
+    addWidgetInContainer(getEmojiSlider(widget));
+  }
+  if (widget.kind === 'cheer-meter') {
+    addWidgetInContainer(getCheerMeter(widget));
+  }
+  if (widget.kind === 'alert') {
+    setAlertWidget(widget);
+  }
+  if (widget.kind === 'video-alert') {
+    setVideoAlertWidget(widget);
+  }
 };
 
 const createWidgetEventHandler = (e) => {
-    console.log(e.event);
-    widgetHandler(e.widgetPayload);
+  console.log(e.event);
+  widgetHandler(e.widgetPayload);
 };
 
 const setupWidgetListener = () => {
-    loadLastPublishedWidget(programId);
-    LiveLike.addWidgetListener({ programId: programId }, createWidgetEventHandler);
+  loadLastPublishedWidget(programId);
+  LiveLike.addWidgetListener(
+    { programId: programId },
+    createWidgetEventHandler
+  );
+};
+
+const setupLeaderboard = (program) => {
+  const buildLeaderboard = (leaderboardId) => {
+    LiveLike.getLeaderboardEntries({
+      leaderboardId,
+    }).then((lb) => {
+      const lbContainer = document.querySelector(
+        '.leaderboard-entries-container'
+      );
+
+      // If leaderboard items already exist, remove them to re-build on leaderboard update
+      lbContainer.children.length > 0 &&
+        Array.from(lbContainer.children).forEach((el) => el.remove());
+
+      // Loop through leaderboard entries to create list items for each entry
+      lb.entries = lb.entries.slice(0, 10);
+      lb.entries.forEach((entry) => {
+        const entryRow = document.createElement('tr');
+        entryRow.setAttribute('class', 'list-item');
+        entryRow.innerHTML = `
+            <td class="rank">${entry.rank}</td>
+            <td class="name">${entry.profile_nickname}</td>
+            <td class="pts">${entry.score}</td>
+          `;
+        lbContainer.appendChild(entryRow);
+      });
+    });
+  };
+  const leaderboardId = '9af81022-8a85-4511-bb7d-2b74934efb93';
+  if (leaderboardId) {
+    buildLeaderboard(leaderboardId);
+    // When a widget is dismissed, we update the leaderboard to show updated ranks and points
+    document.addEventListener('beforewidgetdetached', () => {
+      buildLeaderboard(leaderboardId);
+    });
+  }
 };
 
 const initLiveLike = (clientId, program) => {
-    return LiveLike.init({
-        clientId: clientId,
-    }).then((profile) => {
-        setupTheme();
-        setupWidgetListener();
-    });
-}
+  return LiveLike.init({
+    clientId: clientId,
+  }).then((profile) => {
+    setupTheme();
+    setupWidgetListener();
+    setupLeaderboard(program);
+  });
+};
 
 const init = (clientId, programId) => {
-    fetch("https://cf-blast.livelikecdn.com/api/v1/programs/" + programId + "/")
-        .then((p) => p.json())
-        .then((program) => {
-            return initLiveLike(clientId, program);
-        });
+  fetch('https://cf-blast.livelikecdn.com/api/v1/programs/' + programId + '/')
+    .then((p) => p.json())
+    .then((program) => {
+      return initLiveLike(clientId, program);
+    });
 };
